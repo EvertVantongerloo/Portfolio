db8143ac4771af7077c9a2a294ef217df22d9a05
EvertVantongerloo <165608170+EvertVantongerloo@users.noreply.github.com>
2024-04-06 02:39:14 +0200

    added text underneath the page

--- index.js
+++ index.js
@@ -52,3 +52,57 @@ function playVideo(videoSrc, containerId) {
     clickedImage.style.display = 'block';
   };
 }
+
+
+
+
+
+
+/* current time */
+
+function updateTime() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  
+  // Add leading zeros if needed
+  minutes = (minutes < 10 ? '0' : '') + minutes;
+  seconds = (seconds < 10 ? '0' : '') + seconds;
+  
+  var timeString = hours + ':' + minutes + ':' + seconds;
+  
+  document.getElementById('current-time').innerText = timeString;
+}
+
+// Update time every second
+setInterval(updateTime, 1000);
+
+// Call initially to display the time immediately
+updateTime();
+
+
+
+
+
+
+/* current date */
+
+function updateDate() {
+  var currentDate = new Date();
+  var year = currentDate.getFullYear();
+  var month = currentDate.getMonth() + 1; // Month is zero-based
+  var day = currentDate.getDate();
+  
+  // Add leading zeros if needed
+  month = (month < 10 ? '0' : '') + month;
+  day = (day < 10 ? '0' : '') + day;
+  
+  var dateString = day + '/' + month + '/' + year;
+  
+  document.getElementById('current-date').innerText = dateString;
+}
+
+// Call initially to display the date immediately
+updateDate();
+

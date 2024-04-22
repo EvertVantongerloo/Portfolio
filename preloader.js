b380ba1e45ec149e02217938aff19b1a9a27df90
EvertVantongerloo <165608170+EvertVantongerloo@users.noreply.github.com>
2024-04-23 01:03:56 +0200

    preloader animation

--- preloader.js
+++ preloader.js
@@ -1,7 +1,20 @@
-var loader = document.querySelector(".loaderContainer")
+var loader = document.querySelector(".loaderContainer");
 
-window.addEventListener("load", vanish);
-
+// Function to make the loader disappear after 5 seconds
 function vanish() {
-  loader.classList.add("dissapear")
+  // Prevent scrolling
+  document.body.style.overflow = 'hidden';
+
+  // Set a timeout to make the loader disappear after 5 seconds
+  setTimeout(function() {
+    loader.classList.add("disappear");
+    
+    // Allow scrolling after the animation finishes
+    setTimeout(function() {
+      document.body.style.overflow = '';
+    }, 800); // Adjust this timeout to match the duration of your animation
+  }, 3100); // 3500 milliseconds = 3.5 seconds
 }
+
+// Call the vanish function when the window is loaded
+window.addEventListener("load", vanish);
